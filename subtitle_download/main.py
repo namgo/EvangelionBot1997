3c761260e9360f306fc278e9378c89c8c1a605a0
Nahum Del Toro <nahumdeltoro@riseup.net>
2019-05-27 19:23:43 -0400

    typo

--- subtitle_download/main.py
+++ subtitle_download/main.py
@@ -78,4 +78,4 @@ except:
 print('hit Ctrl-c when you don\' see any more subtitles being grabbed')
 seasons = sys.argv[4].split('-')
 episodes = sys.argv[5].split('-')
-get_all_seasons(token, server, sys.argv[3], range(int(seasons[0]), int(seasons[1]+1)), range(int(episodes[0]), int(episodes[1]+1)))
+get_all_seasons(token, server, sys.argv[3], range(int(seasons[0]), int(seasons[1])+1), range(int(episodes[0]), int(episodes[1])+1))
